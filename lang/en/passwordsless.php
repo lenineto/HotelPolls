4ee3ab2035b11ba560e585bbf448e86a67fa6322
Leni Neto <leni@cyber5.net>
2024-03-05 22:22:00 +0000

    - Adding localization - Localizing passwordless messages

---
+++ lang/en/passwordsless.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | PasswordLess Login Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'sent' => 'Login link sent to inbox.',
+    'throttled' => 'Login link was already sent. Please check your inbox or try again later.',
+    'invalid_token' => 'Invalid link supplied. Please request new one.',
+    'invalid_user' => 'Invalid user info supplied.',
+    'verified' => 'Login successful.',
+];
